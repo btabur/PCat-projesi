db30cc576045ab63aa5fdc7772efeab3e5e3db3b
Balı Tabur <webtasarim.0323@gmail.com>
2024-01-31 00:03:41 +0300

    database addition and data extraction operations were done

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs')
 const path = require('path');
+const Photo = require('./models/Photo');
+
 
 const app = express();
+
+//connect db
+mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db');
 
 //template engine
 app.set("view engine","ejs")
 
 //middlewares
-app.use(express.static('public'))
+app.use(express.static('public'));
+//alınan requestleri sonlandırır
+app.use(express.urlencoded({extended :true}))  //url deki data yı okumamıza yardım eder
+app.use(express.json()) //url deki data yı json formatına çevirir
 
 //routes
-app.get('/', (req, res) => {
-
-  res.render('index')
+app.get('/', async(req, res) => {
+    const photos = await Photo.find({})
+  res.render('index',{
+    photos
+  })
 });
 
 app.get('/about', (req, res) => {
@@ -24,6 +35,12 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
 
   res.render('add')
+});
+
+app.post('/photos', async(req, res) => {
+
+  await Photo.create(req.body)
+  res.redirect('/')
 });
 
 
--- test.js
+++ test.js
@@ -7,9 +7,6 @@ const Schema = mongoose.Schema;
 //mongoose.connect('mongodb://localhost:27017/pcat-test-db');
 mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db');
 
-
-
-
 //create schema
 
 const PhotoSchema = new Schema({
@@ -21,7 +18,30 @@ const Photo = mongoose.model('Photo', PhotoSchema);
 
 //create a photo
 
-Photo.create({
-  tittle: 'Photo tittle 1',
-  description: 'Photo desc 1',
-});
+// Photo.create({
+//   tittle: 'Photo tittle 2',
+//   description: 'Photo desc 2',
+// });
+
+//read
+
+// Photo.find().then((data)=> {
+//   console.log(data)
+// })
+const id = '65b93c505115e6d6c3f87ceb';
+//update
+// 
+
+// Photo.findByIdAndUpdate(
+//   id,
+//   {
+//     tittle: 'Photo uptaded 2 ',
+//     description: 'Desc uptated 2 ',
+//   },
+//   { new: true }   // güncellenen veriyi döndürmesi için
+// ).then((res) => console.log(res));
+
+
+//delete
+
+Photo.findByIdAndDelete(id).then(()=> console.log('silme islemi yapıldı') )
